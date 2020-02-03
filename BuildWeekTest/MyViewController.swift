ce7f56bf0c70ca7a70a900a9c4a2ba2d707d7508
Dan M <djmthedev@gmail.com>
2020-02-03 09:59:53 -0800

    added other print

---
+++ BuildWeekTest/MyViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MyViewController.swift
+//  BuildWeekTest
+//
+//  Created by morse on 2/3/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Howdy!")
+        print("Ben is cool")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
